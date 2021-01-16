560a75ed0b303271975da1c6bcf6afe47fffa12f
Muhammad Nur Hika Pasca <hikapasca@gmail.com>
2021-01-16 23:38:05 +0700

    - convert config.json to config.js - add env variable on config.js

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "myPokemon",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "postgres",
+    password: "postgres",
+    database: "myPokemon_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.USERNAME_PRODUCTION,
+    password: process.env.PASSWORD_PRODUCTION,
+    database: process.env.DATABASE_PRODUCTION,
+    host: process.env.HOST_PRODUCTION,
+    dialect: process.env.DIALECT_PRODUCTION,
+  },
+};
